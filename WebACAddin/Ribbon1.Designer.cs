9eacf466b86928bf30de1479276bf7ce7a96d137
msuzuki <led.centipede@gmail.com>
2018-08-11 17:18:04 +0900

    first commit

---
+++ WebACAddin/Ribbon1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WebACAddin
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナーのサポートに必要なメソッドです。
+        /// このメソッドの内容をコード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webACTab = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.webACTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webACTab
+            // 
+            this.webACTab.Groups.Add(this.group1);
+            this.webACTab.Label = "WebAC";
+            this.webACTab.Name = "webACTab";
+            // 
+            // group1
+            // 
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.webACTab);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.webACTab.ResumeLayout(false);
+            this.webACTab.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab webACTab;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
