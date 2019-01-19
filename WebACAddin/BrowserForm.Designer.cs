169569c311735291bad4d9391dc459f670c0a148
msuzuki <led.centipede@gmail.com>
2019-01-19 19:53:50 +0900

    簡易HTMLプレビュー機能の機能改善
    アクティブセル値でプレビュー更新可能にした

--- WebACAddin/BrowserForm.Designer.cs
+++ WebACAddin/BrowserForm.Designer.cs
@@ -28,32 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.browserControl = new System.Windows.Forms.WebBrowser();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.previewHtmlUpdateButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.browserControl, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.13738F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.86262F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(587, 313);
+            this.tableLayoutPanel1.TabIndex = 0;
             // 
             // browserControl
             // 
             this.browserControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.browserControl.Location = new System.Drawing.Point(0, 0);
+            this.browserControl.Location = new System.Drawing.Point(3, 3);
             this.browserControl.MinimumSize = new System.Drawing.Size(20, 20);
             this.browserControl.Name = "browserControl";
-            this.browserControl.Size = new System.Drawing.Size(587, 261);
+            this.browserControl.Size = new System.Drawing.Size(581, 273);
             this.browserControl.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.Controls.Add(this.previewHtmlUpdateButton);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(374, 282);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(210, 28);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // previewHtmlUpdateButton
+            // 
+            this.previewHtmlUpdateButton.Location = new System.Drawing.Point(3, 3);
+            this.previewHtmlUpdateButton.Name = "previewHtmlUpdateButton";
+            this.previewHtmlUpdateButton.Size = new System.Drawing.Size(128, 23);
+            this.previewHtmlUpdateButton.TabIndex = 0;
+            this.previewHtmlUpdateButton.Text = "セル値でプレビュー更新";
+            this.previewHtmlUpdateButton.UseVisualStyleBackColor = true;
+            this.previewHtmlUpdateButton.Click += new System.EventHandler(this.previewHtmlUpdateButton_Click);
             // 
             // BrowserForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 261);
-            this.Controls.Add(this.browserControl);
+            this.ClientSize = new System.Drawing.Size(587, 313);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "BrowserForm";
             this.Text = "Browser";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         public System.Windows.Forms.WebBrowser browserControl;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button previewHtmlUpdateButton;
     }
 }
--- WebACAddin/BrowserForm.cs
+++ WebACAddin/BrowserForm.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
 namespace WebACAddin
 {
     public partial class BrowserForm : Form
@@ -14,6 +18,34 @@ namespace WebACAddin
         public BrowserForm()
         {
             InitializeComponent();
+        }
+
+        //アクティブセルのデータを取得
+        private void preview_update()
+        {
+            Excel.Range sa = Globals.ThisAddIn.Application.ActiveCell;
+            Excel.Worksheet ash = Globals.ThisAddIn.Application.ActiveSheet;
+
+            string prefix = "<!doctype html><html lang='ja'><head><meta charset='utf-8'></head><body>";
+            string body = "";
+            string sufix = "</body></html>";
+
+            int r, c = 0;
+            r = sa.Row;
+            c = sa.Column;
+            if (ash.Cells[r, c].Value == null) return;
+            Type t = ash.Cells[r, c].Value.GetType();
+            if (t.Equals(typeof(string)))
+            {
+                body = (string)ash.Cells[r, c].Value;
+            }
+            browserControl.DocumentText = prefix + body + sufix;
+        }
+
+        //セル値から更新ボタンをクリック
+        private void previewHtmlUpdateButton_Click(object sender, EventArgs e)
+        {
+            preview_update();
         }
     }
 }
